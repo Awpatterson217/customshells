a0a15d2253282170cd56201f83d429314f8564aa
Adam Patterson <awpatterson217@gmail.com>
2017-09-01 23:22:39 -0500

    Patch 1.0.7

--- test/test.js
+++ test/test.js
@@ -1,10 +1,6 @@
 "use strict";
 
 const customshell = require('../');
-const Tree = require('../').Tree;
-
-// WORKS
-let myTree = new Tree();
 
 // WORKS
 let myShell = customshell.createShell();
@@ -51,68 +47,21 @@ myShell
     //.tree(root)
     //.create();
 
-/*    
-let child = myShell
-                .at(link) 
-                .toFile('mylogs.txt') 
-                .setOptions()                    
-                .create(); 
 
-let myRef = myShell
-                .node('myModule.js')
-                .create();
+//let child = myShell
+                //.at(link) 
+                //.toFile('mylogs.txt') 
+                //.setOptions()                    
+                //.create(); 
+
+//let myRef = myShell
+                //.node('myModule.js')
+                //.create();
 
 console.log(myRef.pid);
 
+/*    RANDOM
 
-
-// Works
-//myTree.getBranch(root, ignorees);
-//myTree.getLeaves(root, Extensions);
-
-//myTree.fertilize(int); // TODO?
-
-// LEAVES
-myTree.on('file', (file, dir, extension) =>{
-    console.log("File: "      + file);
-    console.log("dir: "       + dir);
-    console.log("Extension: " + extension);
-});
-
-// LEAVES
-myTree.on('dirFound', directory =>{
-    console.log("directory: " + directory);
-});
-
-// LEAVES
-myTree.on('autumn', (errors, files, extensionsMatched) =>{
-    console.log("Files found: "           + files);
-    console.log("Number of Files found: " + files.length);
-    console.log("Extensions Matched: "    + extensionsMatched);
-    if(errors.length){
-        console.log("Number of errors: "  + errors.length);
-        console.log("Reasons for error: " + errors[0].msg);
-        console.log("Location of error: " + errors[0].path);
-    }
-});
-
-// BRANCH
-myTree.on('dir', directory =>{
-    console.log("directory: " + directory);
-});
-
-// BRANCH 
-myTree.on('gathered', (errors, directories) => {
-    console.log("Directories Found: "           + directories);
-    console.log("Number of Directories Found: " + directories.length);    
-    if(errors.length){
-        console.log("Number of errors: "  + errors.length);
-        console.log("Reasons for error: " + errors[0].msg);
-        console.log("Location of error: " + errors[0].path);
-    }
-});
-
-// RANDOM
 console.log("process.cpuUsage keys" + Object.keys(process.cpuUsage));
 console.log(process.cpuUsage.user);
 console.log(process.cpuUsage.system);
