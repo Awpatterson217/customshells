443d6ac4d9bc536ee0108e755eb4e60511370a02
awpatterson217 <awpatterson217@gmail.com>
2018-05-17 20:32:54 -0500

    fixing create method name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**                                             2017
  * @fileOverview A library to simplify the use of shells in Node.js
  * @author <a href="mailto:awpatterson217@gmail.com">Adam Patterson</a>
- * @version 3.0.0
+ * @version 3.0.1
  *                              Tested on Windows, Node.js v8.
  */
 
